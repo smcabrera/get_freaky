dc66fe55423dd9ea4d47289757c171599f50f0aa
Stephen Mariano Cabrera <stephen.m.cabrera@gmail.com>
2015-05-07 12:38:54 -0500

    Improve description and add link to code of conduct to readme

--- lib/get_freaky/info.rb
+++ lib/get_freaky/info.rb
@@ -1,4 +1,4 @@
 module Info
   DATE = %q{2015-5-7}
-  DESCRIPTION = %q{Wrapper for the confreaks API. Makes it easier to browse and download videos from conventions posted on confreaks.tv.}
+  DESCRIPTION = %q{Wrapper for the confreaks API. Browse and download convention videos from the command line.}
 end
